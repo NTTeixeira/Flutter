93989b58948a6e27bd2d8e05e881b78d37d91386
Nathan Siman Teixeira <64165617+NTTeixeira@users.noreply.github.com>
2021-11-23 22:48:42 -0300

    Add files via upload

---
+++ Exercícios-Faculdade/ex18/ex04/main.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MeuAplicativo());
+class MeuAplicativo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: PrimeiraRota(),
+
+    );
+  }
+}
+class PrimeiraRota extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Primeira Rota'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+          UserAccountsDrawerHeader(
+          decoration: BoxDecoration(
+          color: Colors.amber,
+          ),
+          accountName: Text("Nathan"),
+          accountEmail: Text("nathan@nathan.com.br"),
+          currentAccountPicture: CircleAvatar(
+            backgroundColor: Colors.white,
+            child: Text(
+              "A",
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.red,
+              ),
+            ),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.video_collection),
+          title: Text("Rota 02"),
+          subtitle: Text("Siga para a Rota 02."),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: () {
+            print('Ir para a Rota 02.');
+            Navigator.push(
+                context, 
+                MaterialPageRoute(builder: (context) => SegundaRota()),
+            );
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.analytics),
+          title: Text("Rota 03"),
+          subtitle: Text("Siga para a Rota 03"),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: () {
+            print('Ir para a Rota 03.');
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => TerceiraRota()),
+            );
+          },
+        ),
+        ListTile(
+        leading: Icon(Icons.home),
+    title: Text("Rota 01"),
+          subtitle: Text("Voltar para a Rota 01"),
+          trailing: Icon(Icons.arrow_forward),
+          onTap: () {
+            print('Voltar para a Rota 01.');
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PrimeiraRota()),
+            );
+          },
+        ),
+            ],
+          ),
+        ),
+      body: Center(
+        child: const Text('Corpo'),
+      ),
+    );
+  }
+}
+
+class SegundaRota extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Segunda Rota'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              accountName: Text("Nathan"),
+              accountEmail: Text("nathan@nathan.com.br"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  "A",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.video_collection),
+              title: Text("Rota 02"),
+              subtitle: Text("Siga para a Rota 02."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('Ir para a Rota 02.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SegundaRota()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.analytics),
+              title: Text("Rota 03"),
+              subtitle: Text("Siga para a Rota 03"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('Ir para a Rota 03.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TerceiraRota()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Rota 01"),
+              subtitle: Text("Voltar para a Rota 01"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('Voltar para a Rota 01.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PrimeiraRota()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Ir para a Primeira Rota'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class TerceiraRota extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Terceira Rota'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              accountName: Text("Nathan"),
+              accountEmail: Text("nathan@nathan.com.br"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text(
+                  "A",
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.video_collection),
+              title: Text("Rota 02"),
+              subtitle: Text("Siga para a Rota 02."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('Ir para a Rota 02.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SegundaRota()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.analytics),
+              title: Text("Rota 03"),
+              subtitle: Text("Siga para a Rota 03"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('Ir para a Rota 03.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TerceiraRota()),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("Rota 01"),
+              subtitle: Text("Voltar para a Rota 01"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print('Voltar para a Rota 01.');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PrimeiraRota()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      backgroundColor: Colors.purple[900],
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Ir para a Primeira Rota'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
+  }
+}
